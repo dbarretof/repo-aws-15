c6c48188b19b949b4bec011c5bafc93f40e60e4d
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2024-09-11 16:29:09 +0000

    version1

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("register.thml")
+
+if __name__ == "__main__":
+    host = "127.0.0.1"
+    port = 8000
+    app.run(host, port, True)
+
+
